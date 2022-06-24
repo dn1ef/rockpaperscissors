784e462c49e8caa2013b285794fcc9d4facdeeec
danny1ef <d.nguyen24@yahoo.com>
2022-06-23 18:16:31 -0600

    Edit js-file.js

--- scripts/js-file.js
+++ scripts/js-file.js
@@ -2,11 +2,11 @@ let playerChoice;
 let cpuChoice = [{choice: 'rock', value: 1 }, {choice: 'paper', value: 2 }, {choice: 'scissors', value: 3 }];
 
 function computerPlay() {
-    return cpuChoice[Math.floor(Math.random() * cpuChoice.length) + 1];
+    return cpuChoice[Math.floor(Math.random() * cpuChoice.length)];
 }
 
 function determineWinner(player, cpu) {
-    let roundCompare = `${player}-${cpuChoice.value}` 
+    let roundCompare = `${player}-${cpu.value}` 
     let winnerCombo = ['1-3', '2-1', '3-2'];
     let win = 0;
 
@@ -17,12 +17,12 @@ function determineWinner(player, cpu) {
         }
     }
 
-    if (player === cpu && win != 1) {
+    if (player === cpu.value && win != 1) {
         return `Tied!`;
     } else if (win != 1) {
-        return `You lost! ${cpu.choice} beats ${playerChoice}`;
+        return `You lost! ${cpu.choice[0].toUpperCase() + (cpu.choice).slice(1)} beats ${playerChoice}.`;
     } else if (win === 1) {
-        return `You win! ${playerChoice} beats ${cpu.choice}`;
+        return `You win! ${playerChoice[0].toUpperCase() + playerChoice.slice(1)} beats ${cpu.choice}.`;
     } else {
         return `Error, could not determine winner of round.`;
     }
@@ -45,13 +45,32 @@ function determineSelection(player) {
     }
 }
 
+function cpuSelection(cpu) {
+    switch(cpu) {
+        case 1:
+            return 'rock';
+            break;
+        case 2:
+            return 'paper';
+            break;
+        case 3:
+            return 'scissors';
+            break;
+        default:
+            return "Error, could not figure out CPU choice."
+    }               
+}
+
+
 function playRound(playerSelection) {
     playerChoice = playerSelection;
-    let cpu = computerPlay();
     let player = determineSelection(playerSelection); 
 
-    return determineWinner(player, cpu);
-
+    if (player == 1 || player == 2 || player == 3) {
+        return determineWinner(player, computerPlay());
+    } else {
+        return "Error - only type 'rock', 'paper', or 'scissors'.";
+    }      
 }
 
 
@@ -59,4 +78,5 @@ function game() {
     for (let i = 0; i < 5; i++) {
        console.log(playRound(prompt('Rock, Paper, or Scissors?'))); 
     }
+    return "Game over!";
 }
